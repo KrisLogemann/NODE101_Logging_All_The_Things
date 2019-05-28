dfddbd8569d0892cef406bc13b15f1076144dbd5
Kris Logemann <krisrlogemann@gmail.com>
2019-05-28 10:21:42 -0700

    adds initial project files

---
+++ README.md
@@ -0,0 +1,9 @@
+May 28th, 2019 10:15 AM
+
+This is my fourth project in my Node101 curriculum at San Diego Code School.
+
+This project involves using node to: 1.) Create a logger and 2.) Practice manipulating data.
+
+Thanks for checking out my code on GitHub.
+
+Kris Logemann
--- server/app.js
+++ server/app.js
@@ -1,20 +1,74 @@
-const express = require('express');
-const fs = require('fs');
+const express = require("express");
 const app = express();
 
+const fs = require("fs");
+const path = require("path");
+const morgan = require("morgan");
+const moment = require("moment");
+let object = [];
+
 app.use((req, res, next) => {
-// write your logging code here
+    // write your logging code here
+    let Agent = req.headers["user-agent"];
+    let Time = moment().format();
+    let Method = req.method;
+    let Resource = req._parsedUrl["path"];
+    let Version = "HTTP/1.1";
+    let Status = "200";
+    console.log(
+        Agent +
+        "," +
+        Time +
+        "," +
+        Method +
+        "," +
+        Resource +
+        "," +
+        Version +
+        "," +
+        Status
+        );
+        
+        var userInfo =
+        "/n" +
+        Agent +
+        "," +
+        Time +
+        "," +
+        Method +
+        "," +
+        Resource +
+        "," +
+        Version +
+        "," +
+        Status;
+        
+        var userInfo = {
+            Agent: req.headers["user-agent"],
+            Time: moment()
+            .utc()
+            .format(),
+            Method: req.method,
+            Resource: req._parsedUrl["path"],
+            Version: "HTTP/1.1",
+            Status: "200"
+        };
+        
+        object.push(userInfo);
+        fs.appendFile(path.resolve(__dirname, 'log.csv'), userInfo, function (err) {
+            if (err) throw err;
+        });
+    next();
+    });
 
+app.get("/", (req, res) => {
+  // write your code to respond "ok" here
+  res.status(200).send("ok");
 });
 
-app.get('/', (req, res) => {
-// write your code to respond "ok" here
-
-});
-
-app.get('/logs', (req, res) => {
-// write your code to return a json object containing the log data here
-
+app.get("/logs", (req, res) => {
+  // write your code to return a json object containing the log data here
+  res.json(object);
 });
 
 module.exports = app;
